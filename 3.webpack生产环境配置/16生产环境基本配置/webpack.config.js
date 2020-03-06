76a1d289c8f72c32bb88a49970d23747d0afe355
zjoney <zdjoey@126.com>
2020-03-06 11:14:25 +0800

    productiion envirment

---
+++ 3.webpack生产环境配置/16生产环境基本配置/webpack.config.js
@@ -0,0 +1,121 @@
+/* eslint-disable linebreak-style */
+/* eslint-disable global-require */
+/* eslint-disable comma-dangle */
+/* eslint-disable linebreak-style */
+const { resolve } = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const OptimizeCssAssetsWebpackPlugin = require('optimize-css-assets-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+// 定义Nodejs环境变量： 决定使用browersers在什么环境
+// process.env.NODE_ENV = 'development';
+
+// 复用loader
+const commonCssLoader = [
+  // 'style-loader',
+  MiniCssExtractPlugin.loader,
+  'css-loader',
+  {
+    loader: 'postcss-loader',
+    options: {
+      plugins: [
+        require('autoprefixer')
+      ]
+    }
+  }
+];
+
+module.exports = {
+  entry: './src/js/index.js',
+  output: {
+    filename: 'js/built.js',
+    path: resolve(__dirname, 'built'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          ...commonCssLoader
+        ]
+      },
+      {
+        test: /\.less$/,
+        use: [
+          ...commonCssLoader,
+          'less-loader',
+        ]
+      },
+      /**
+       * 正常来说一个文件被一个loader处理
+       * 当需要被多个loader处理，那么需要指定loader先后顺序
+       * 先eslint 接着babel
+       */
+      {
+        // package.json中eslintConfig --> airbnb
+        test: /\.js$/,
+        exclude: 'node_modules',
+        // 优先执行，代码位置无关
+        enforce: 'pre',
+        loader: 'eslint-loader',
+        options: {
+          fix: true
+        }
+      },
+      {
+        test: /\.js$/,
+        exclude: 'node_modules',
+        loader: 'babel-loader',
+        options: {
+          presets: [
+            [
+              '@babel/preset-ev', {
+                useBuiltIns: 'usage',
+                corejs: { version: 3 },
+                targets: {
+                  chrome: '60',
+                  ie: '9',
+                }
+              }
+            ]
+          ]
+        }
+      },
+      {
+        test: /\.(jpg|png|gif)$/,
+        loader: 'url-loader',
+        options: {
+          limit: 8 * 1024,
+          name: '[hash:10].[ext]',
+          outputPath: 'imgs',
+          esModule: false,
+        }
+      },
+      {
+        test: /\.html$/,
+        loader: 'html-loader',
+      },
+      {
+        exclude: /\.(js|css|less"|jpg|png|gif)$/,
+        loader: 'file-loader',
+        options: {
+          outputPath: 'media'
+        }
+      }
+    ]
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: 'css/built.css'
+    }),
+    new OptimizeCssAssetsWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+      minify: {
+        collapseInlineTagWhitespace: true,
+        removeComments: true,
+      }
+    })
+  ],
+  mode: 'production',
+};
