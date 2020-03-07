61b7c3c59c4b399105c67c8cc48e2778afdf6e1f
zjoney <zdjoey@126.com>
2020-03-07 22:23:37 +0800

    tree

---
+++ 4.webpack优化环境配置/22tree_shaking/webpack.config.js
@@ -0,0 +1,135 @@
+/* eslint-disable linebreak-style */
+/* eslint-disable global-require */
+/* eslint-disable comma-dangle */
+/* eslint-disable linebreak-style */
+const { resolve } = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const OptimizeCssAssetsWebpackPlugin = require('optimize-css-assets-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+
+// process.env.NODE_ENV = 'development';
+/**
+ * tree shaking 去除无用代码
+ * 前提： 1.必须使用ES6模块化, 2.开启production环境
+ * 作用：减少代码体积
+ * 
+ * 在pakage.json中配置
+ *  "sideEffects": false,所有代码都没有副作用(都可以进行tree shaking)
+ *    问题： 可能会把css / @babel/polyfill干掉
+ *    解决："sideEffects": ["*.css", "*.less"]
+ */
+const commonCssLoader = [
+  // 'style-loader',
+  MiniCssExtractPlugin.loader,
+  'css-loader',
+  {
+    loader: 'postcss-loader',
+    options: {
+      plugins: [
+        require('autoprefixer')
+      ]
+    }
+  }
+];
+
+module.exports = {
+  entry: './src/js/index.js',
+  output: {
+    filename: 'js/built.[contenthash:10].js',
+    path: resolve(__dirname, 'build'),
+  },
+  module: {
+    rules: [
+      {
+        // 在package.json中eslintConfig --> airbnb
+        test: /\.js$/,
+        exclude: /node_modules/,
+        // 优先执行
+        enforce: 'pre',
+        loader: 'eslint-loader',
+        options: {
+          fix: true
+        }
+      },
+      {
+        // 以下loader只会匹配一个
+        // 注意：不能有两个配置处理同一种类型文件
+        oneOf: [
+          {
+            test: /\.css$/,
+            use: [...commonCssLoader]
+          },
+          {
+            test: /\.less$/,
+            use: [...commonCssLoader, 'less-loader']
+          },
+          /*
+            正常来讲，一个文件只能被一个loader处理。
+            当一个文件要被多个loader处理，那么一定要指定loader执行的先后顺序：
+              先执行eslint 在执行babel
+          */
+          {
+            test: /\.js$/,
+            exclude: /node_modules/,
+            loader: 'babel-loader',
+            options: {
+              presets: [
+                [
+                  '@babel/preset-env',
+                  {
+                    useBuiltIns: 'usage',
+                    corejs: { version: 3 },
+                    targets: {
+                      chrome: '60',
+                      firefox: '50'
+                    }
+                  }
+                ]
+              ],
+              // 开启babel缓存
+              // 第二次构建时，会读取之前的缓存
+              cacheDirectory: true
+            }
+          },
+          {
+            test: /\.(jpg|png|gif)/,
+            loader: 'url-loader',
+            options: {
+              limit: 8 * 1024,
+              name: '[hash:10].[ext]',
+              outputPath: 'imgs',
+              esModule: false
+            }
+          },
+          {
+            test: /\.html$/,
+            loader: 'html-loader'
+          },
+          {
+            exclude: /\.(js|css|less|html|jpg|png|gif)/,
+            loader: 'file-loader',
+            options: {
+              outputPath: 'media'
+            }
+          }
+        ]
+      }
+    ]
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: 'css/built.[contenthash:10].css'
+    }),
+    new OptimizeCssAssetsWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+      minify: {
+        collapseInlineTagWhitespace: true,
+        removeComments: true,
+      }
+    })
+  ],
+  mode: 'production',
+  devtool: 'source-map'
+};
