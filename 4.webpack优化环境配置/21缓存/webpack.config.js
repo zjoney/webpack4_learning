5051ae1c55d77ec98bef845b8779b24129f4eeb1
zjoney <zdjoey@126.com>
2020-03-07 22:21:16 +0800

    webpack

---
+++ 4.webpack优化环境配置/21缓存/webpack.config.js
@@ -0,0 +1,138 @@
+/* eslint-disable linebreak-style */
+/* eslint-disable global-require */
+/* eslint-disable comma-dangle */
+/* eslint-disable linebreak-style */
+const { resolve } = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const OptimizeCssAssetsWebpackPlugin = require('optimize-css-assets-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+
+// process.env.NODE_ENV = 'development';
+/**
+ * babel缓存
+ *   cacheDirectory: true
+ *    --> 第二次打包速度更快
+ * 文件资源缓存
+ *    hash: 每次webpack构建生成一个Hash值
+ *        问题： css或者js同时使用一个hash值，如果重新打包，导致缓存失败
+ *    chunkhash: 根据chunk生成Hash,如果打包来源于同一个chunk，那么hash就一样
+ *        问题： css或者js还是一样的hash值
+ *        因为css 在js中被引入，属于一个chunk
+ *    contenthash: 根据文件内容生成不同hash值
+ *    --> 让代码上线运行缓存更好使用
+ */
+const commonCssLoader = [
+  // 'style-loader',
+  MiniCssExtractPlugin.loader,
+  'css-loader',
+  {
+    loader: 'postcss-loader',
+    options: {
+      plugins: [
+        require('autoprefixer')
+      ]
+    }
+  }
+];
+
+module.exports = {
+  entry: './src/js/index.js',
+  output: {
+    filename: 'js/built.[contenthash:10].js',
+    path: resolve(__dirname, 'build'),
+  },
+  module: {
+    rules: [
+      {
+        // 在package.json中eslintConfig --> airbnb
+        test: /\.js$/,
+        exclude: /node_modules/,
+        // 优先执行
+        enforce: 'pre',
+        loader: 'eslint-loader',
+        options: {
+          fix: true
+        }
+      },
+      {
+        // 以下loader只会匹配一个
+        // 注意：不能有两个配置处理同一种类型文件
+        oneOf: [
+          {
+            test: /\.css$/,
+            use: [...commonCssLoader]
+          },
+          {
+            test: /\.less$/,
+            use: [...commonCssLoader, 'less-loader']
+          },
+          /*
+            正常来讲，一个文件只能被一个loader处理。
+            当一个文件要被多个loader处理，那么一定要指定loader执行的先后顺序：
+              先执行eslint 在执行babel
+          */
+          {
+            test: /\.js$/,
+            exclude: /node_modules/,
+            loader: 'babel-loader',
+            options: {
+              presets: [
+                [
+                  '@babel/preset-env',
+                  {
+                    useBuiltIns: 'usage',
+                    corejs: { version: 3 },
+                    targets: {
+                      chrome: '60',
+                      firefox: '50'
+                    }
+                  }
+                ]
+              ],
+              // 开启babel缓存
+              // 第二次构建时，会读取之前的缓存
+              cacheDirectory: true
+            }
+          },
+          {
+            test: /\.(jpg|png|gif)/,
+            loader: 'url-loader',
+            options: {
+              limit: 8 * 1024,
+              name: '[hash:10].[ext]',
+              outputPath: 'imgs',
+              esModule: false
+            }
+          },
+          {
+            test: /\.html$/,
+            loader: 'html-loader'
+          },
+          {
+            exclude: /\.(js|css|less|html|jpg|png|gif)/,
+            loader: 'file-loader',
+            options: {
+              outputPath: 'media'
+            }
+          }
+        ]
+      }
+    ]
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: 'css/built.[contenthash:10].css'
+    }),
+    new OptimizeCssAssetsWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+      minify: {
+        collapseInlineTagWhitespace: true,
+        removeComments: true,
+      }
+    })
+  ],
+  mode: 'production',
+  devtool: 'source-map'
+};
