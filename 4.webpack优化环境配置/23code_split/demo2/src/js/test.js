f7f80ae7294f2fcb8007127b7cdae986d2e147be
zjoney <zdjoey@126.com>
2020-03-08 12:55:21 +0800

    test.js asset

---
+++ 4.webpack优化环境配置/23code_split/demo2/src/js/test.js
@@ -0,0 +1,14 @@
+/* eslint-disable linebreak-style */
+/* eslint-disable space-infix-ops */
+/* eslint-disable linebreak-style */
+/* eslint-disable space-before-blocks */
+/* eslint-disable no-unused-vars */
+import $ from 'jquery';
+console.log($);
+
+function mul(a, b) {
+  return a + b;
+}
+function count(a, b){
+  return a-b;
+}
